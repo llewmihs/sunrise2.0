efe71e68cde9bb0c0a6cefca936b829a5b7e34e4
Joe <joeshimwell@gmail.com>
2020-04-14 22:41:29 +0100

    asd

--- full_day_backup.py
+++ full_day_backup.py
@@ -1,13 +1,11 @@
 import subprocess # to run file cleanup after the upload
-from creds import *    # my dropbox API key and Push bullet API key
+from config import *    # my dropbox API key and Push bullet API key
 from time import strftime
 from glob import glob
 
 full_file_list = glob("/home/pi/sunrise2.0/images/*.JPG")
-folder_name = strftime("/imagecache/%d-%b-%Y-")
-
-
+folder_name = strftime("/imagecache/%d-%b")
 
 for i in full_file_list:
-    target = localpath + folder_name + i[-8:]
-    subprocess.call(f"sshpass -p {password} scp {i} {target}", shell = True)
+    print(f"sshpass -p {password} scp {i} {localpath}{folder_name}")
+    subprocess.call(f"sshpass -p {password} scp {i} {localpath}{folder_name}{i[-8:]}", shell = True)
