934d54d25f2415012cfe8a9ecc20811c6d89fe8b
Joe <joeshimwell@gmail.com>
2020-04-14 22:45:22 +0100

    asd

--- full_day_backup.py
+++ full_day_backup.py
@@ -4,7 +4,7 @@ from time import strftime
 from glob import glob
 
 full_file_list = glob("/home/pi/sunrise2.0/images/*.JPG")
-folder_name = strftime("/imagecache/%d-%b")
+folder_name = strftime("/imagecache/%d%m%-y-")
 
 for i in full_file_list:
     print(f"sshpass -p {password} scp {i} {localpath}{folder_name}")
