29a52ab304c94f27deb38deb6f6c041b32ba4f7f
Joe <joeshimwell@gmail.com>
2020-04-15 21:17:39 +0100

    asd

--- file_mover.py
+++ file_mover.py
@@ -3,9 +3,6 @@ from config import *    # my dropbox API key and Push bullet API key
 from time import strftime, sleep
 from glob import glob
 
-
-    subprocess.call(f"sshpass -p {password} scp {i} {localpath}{folder_name}{i[27:]}", shell = True)
-
 def watcher():
     full_file_list = glob("/home/pi/sunrise2.0/images/*.JPG")
     folder_name = strftime("/dayimages/")
