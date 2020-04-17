201086566972a5928d46015a06b8ff38b50943d7
Joe <joeshimwell@gmail.com>
2020-04-17 07:48:16 +0100

    asd

--- sunrise4homepi.py
+++ sunrise4homepi.py
@@ -6,6 +6,7 @@ all_files = sorted(glob("/mnt/myDisk/shared/dayimages/*.JPG"))
 for i in all_files:
     if i == "/mnt/myDisk/shared/dayimages/IMAGE_0600.JPG":
         break
+    print(f"Copying file {i[-8:]}")
     subprocess.call(f"cp {i} /home/pi/sunrise/IMAGE_{i[-8:]}", shell=True)
 
 subprocess.call("/usr/local/bin/ffmpeg -y -r 30 -f image2 -start_number 0000 -i /home/pi/sunrise//IMAGE_%04d.JPG -vcodec libx264 -preset slow -crf 17 /home/pi/timelapses/17-April-sunrise.mp4", shell=True)
